a105016c9805a860574925835ea41d1798e40558
Christian <christianmpacheco@gmail.com>
2024-04-01 20:43:23 -0700

    prob 3

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -22,8 +22,18 @@ console.log(result3); // hi!!!!!
 *******************************************************************************/
 
 function multiMap(val, n, cb) {
-  // Your code here 
+  console.log(val)
+  for(let i = 0; i < n; i++) {
+    val = cb(val)
+    console.log(val)
+  }
+  return val;
 }
+
+let result1 = multiMap(7, 2, function(n) {
+  return n * 10;
+});
+console.log(result1); // 700
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
